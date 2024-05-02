0e799d0198cacda9b5779cec1b16ee9ef09771f2
Stefan May <stefan.may@eduart-robotik.com>
2024-05-02 11:14:17 +0200

    dev version

--- ohm_mecanum_sim/ohm_mecanum_sim_node.py
+++ ohm_mecanum_sim/ohm_mecanum_sim_node.py
@@ -9,29 +9,13 @@
 import pygame
 import rclpy
 from rclpy.node import Node
-#from ohm_mecanum_simulator import Ohm_Mecanum_Simulator
+from ohm_mecanum_sim.ohm_mecanum_simulator import Ohm_Mecanum_Simulator
 
 from std_msgs.msg import String
 
-
-class MinimalPublisher(Node):
-
-    def __init__(self):
-        super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
-        timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
-
-    def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 1
-
 def main(args=None):
     pygame.init()
+    rclpy.init(args=args)
 
     # Screen size
     size = width, height = 1600, 900
@@ -39,7 +23,7 @@ def main(args=None):
     # Drawing surface
     surface = pygame.display.set_mode(size, pygame.HWSURFACE | pygame.DOUBLEBUF)
 
-    #sim = Ohm_Mecanum_Simulator(surface, "ohm_mecanum_sim", "Ohm Mecanum Simulator")
+    sim = Ohm_Mecanum_Simulator(surface, "ohm_mecanum_sim", "Ohm Mecanum Simulator")
 
     #sim.spawn_robot(2, 2, 0, "robot1")
     # sim.spawn_robot(5, 7, 0, "robot2")
@@ -51,12 +35,11 @@ def main(args=None):
 
     #sim.run()
 
-    rclpy.init(args=args)
+    
+    sim.start_scheduler()
+    rclpy.spin(sim)
 
-    minimal_publisher = MinimalPublisher()
-
-    rclpy.spin(minimal_publisher)
-    minimal_publisher.destroy_node()
+    sim.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
--- ohm_mecanum_sim/ohm_mecanum_simulator.py
+++ ohm_mecanum_sim/ohm_mecanum_simulator.py
@@ -6,25 +6,48 @@
 # Description: Pygame-based robot simulator with ROS2 interface
 # ------------------------------------------------------------
 
-import rospy
 import pygame
 import sys
-from std_srvs.srv import SetBool, SetBoolResponse
-from robot import Robot
-from ohm_mecanum_sim.srv import Spawn, Kill, SpawnRequest, SpawnResponse, KillRequest, KillResponse
-from std_msgs.msg import String
 
-class Ohm_Mecanum_Simulator:
+import rclpy
+from rclpy.node import Node
+from ohm_mecanum_sim.srv import Spawn, Kill, SpawnRequest, SpawnResponse, KillRequest, KillResponse
+
+class Ohm_Mecanum_Simulator(Node):
 
     def __init__(self, surface, rosname, windowtitle):
+        super().__init__(rosname)
         self._surface = surface
         self._meter_to_pixel = 100
         self._robots = []
         self._line_segment_obstacles = []
         self._verbose = False
-        rospy.init_node(rosname, anonymous=True)
+        timer_period = 0.05
+        
+        self.create_service('/spawn', Spawn, self.service_callback_spawn)
+
         pygame.display.set_caption(windowtitle)
     
+    def start_scheduler(self):
+        clock = pygame.time.Clock()
+        clock.tick(360)
+        timer_period = 0.02
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+
+
+    def timer_callback(self):
+        bg_color = (64, 64, 255)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.exit_simulation()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_c and pygame.key.get_mods() & pygame.KMOD_CTRL:
+                    self.exit_simulation()
+        self._surface.fill(bg_color)
+
+        pygame.display.update()
+
     def __del__(self):
         pass
 
@@ -32,20 +55,7 @@ class Ohm_Mecanum_Simulator:
         self.spawn_robot(req.x, req.y, req.theta, req.name)
         response = SpawnResponse(req.x, req.y, req.theta, req.name)
         return response
-
-    def service_callback_kill(self, req):
-        self.kill_robot(req.name)
-        response = KillResponse(True)
-        return response
-
-    def service_callback_verbose(self, req):
-        self._verbose = req.data
-        if(self._verbose):
-            msg = "Verbosity increased"
-        else:
-            msg = "Verbosity decreased"
-        return SetBoolResponse(True, msg)
-
+    
     def spawn_robot(self, x, y, theta, name):
         self._robots.append(Robot(x, y, theta, name))
 
@@ -89,87 +99,3 @@ class Ohm_Mecanum_Simulator:
             r.stop()
             del r
         sys.exit()
-
-    def run(self):
-        bg_color = (64, 64, 255)
-        rospy.Service('/spawn', Spawn, self.service_callback_spawn)
-        rospy.Service('/kill', Kill, self.service_callback_kill)
-        rospy.Service('/verbose', SetBool, self.service_callback_verbose)
-        rate = rospy.Rate(50)
-
-        clock = pygame.time.Clock()
-        clock.tick(360)
-
-        while 1:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.exit_simulation()
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_c and pygame.key.get_mods() & pygame.KMOD_CTRL:
-                        self.exit_simulation()
-
-            self._surface.fill(bg_color)
-            
-            # Draw obstacles
-            for obstacle in self._line_segment_obstacles:
-                pixel_segment_start = self.transform_to_pixelcoords(obstacle[0])
-                pixel_segment_end = self.transform_to_pixelcoords(obstacle[1])
-                pygame.draw.line(self._surface, pygame.Color(0, 0, 0), pixel_segment_start, pixel_segment_end, 3)
-        
-            # Convert robot coordinates for displaying all entities in pixel coordinates
-            for r in self._robots:
-
-                r.acquire_lock()
-                # Draw robot symbol
-                coords      = r.get_coords()
-                pixel_robot = self.transform_to_pixelcoords(coords)
-                rect        = r.get_rect()
-                rect.center = pixel_robot
-                rect.move(pixel_robot)
-                self._surface.blit(r.get_image(), rect)
-
-                pos_sensor = r.get_pos_tof()
-                pos_hitpoint = r.get_far_tof()
-
-                # Determine distances to other robots
-                dist_to_obstacles  = []
-                for obstacle in self._robots:
-                    if(obstacle != r):
-                        obstacle_coords = obstacle.get_coords()
-                        dist_to_obstacles = r.get_distance_to_circular_obstacle(obstacle_coords, obstacle.get_obstacle_radius(),  dist_to_obstacles)
-
-                        # Draw circular radius of obstacle
-                        if(self._verbose):
-                            pixel_obstacle = self.transform_to_pixelcoords(obstacle_coords)
-                            obstacle_rect = obstacle.get_rect()
-                            obstacle_rect.center = pixel_obstacle
-                            obstacle_rect.move(pixel_obstacle)
-                            pygame.draw.circle(self._surface, (255, 0, 0), (int(pixel_obstacle[0]), int(pixel_obstacle[1])), int(obstacle.get_obstacle_radius()*self._meter_to_pixel), 1)
-                
-                # Determine distances to line segments
-                for obstacle in self._line_segment_obstacles:
-                    dist_to_obstacles = r.get_distance_to_line_obstacle(obstacle[0], obstacle[1], dist_to_obstacles)
-
-                r.publish_tof(dist_to_obstacles)
-
-                r.release_lock()
-
-                min_dist = 9999
-                for i in range(0, len(dist_to_obstacles)):
-                    if(dist_to_obstacles[i]<min_dist and dist_to_obstacles[i]>0):
-                        min_dist = dist_to_obstacles[i];
-                if(min_dist<(0.2+r._offset_tof)):
-                    r.reset_pose()
-                elif (r._coords[0] < 0 or r._coords[1] < 0 or r._coords[0] > self._surface.get_width()/self._meter_to_pixel or r._coords[1] > self._surface.get_height()/self._meter_to_pixel):
-                    r.reset_pose()
-
-                # Draw ToF beams
-                pos_hitpoint = r.get_hit_tof(dist_to_obstacles)
-                for i in range(0,r.get_tof_count()):
-                    pixel_sensor = self.transform_to_pixelcoords(pos_sensor[i])
-                    pixel_hitpoint = self.transform_to_pixelcoords(pos_hitpoint[i])
-                    pygame.draw.line(self._surface, pygame.Color(255, 0, 0), pixel_sensor, pixel_hitpoint)          
-
-            pygame.display.update()
-
-            rate.sleep()
