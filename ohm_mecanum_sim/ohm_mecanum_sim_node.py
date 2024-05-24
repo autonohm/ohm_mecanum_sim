0ec44229f450e0c05017e22385d949308f638dab
Stefan May <stefan.may@eduart-robotik.com>
2024-05-24 13:36:53 +0200

    removed unnessesary stuff

--- ohm_mecanum_sim/ohm_mecanum_sim_node.py
+++ ohm_mecanum_sim/ohm_mecanum_sim_node.py
@@ -14,23 +14,6 @@ from rclpy.executors import SingleThreadedExecutor
 
 from std_msgs.msg import String
 
-
-class MinimalPublisher(Node):
-
-    def __init__(self):
-        super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
-        timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
-
-    def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 1
-
 def main(args=None):
     pygame.init()
     rclpy.init(args=args)
